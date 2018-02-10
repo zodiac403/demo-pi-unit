d0d3b4b82a71d44991023095aad0dfb0aa71ceeb
zodiac403 <zodiac403@gmx.de>
2018-02-10 14:40:13 +0100

    Create car_test.py

---
+++ car_test.py
@@ -0,0 +1,24 @@
+import unittest
+from car import Car
+
+print Car.__doc__
+
+class CarTest(unittest.TestCase):
+    def setUp(self):
+        self.initialSpeed = 3
+        self.testObject = Car(self.initialSpeed)
+
+    def test_ctor_speed(self):
+        """ctor should initialize speed field"""
+        testObject = Car(9)
+        assert testObject.getSpeed() == 9
+    
+    def test_brake_stop(self):
+        """brake should stop the car"""
+        self.testObject.speed = 30
+        self.testObject.brake()
+        assert self.testObject.speed == 0
+
+
+if __name__ == "__main__":
+    unittest.main() # run all tests
